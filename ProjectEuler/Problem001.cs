0fe39957257957a1fcb6c5cc8fadc3e5a5361448
Lukasz Lepecki <llepecki@users.noreply.github.com>
2019-04-12 22:51:32 +0200

    Problem 1 from Project Euler solved

---
+++ ProjectEuler/Problem001.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+
+namespace ProjectEuler
+{
+    public class Problem001
+    {
+        [Fact]
+        public void Solution()
+        {
+            const int limes = 1000;
+
+            int sum = 0;
+            int divisibleByThree = 3;
+            int divisibleByFive = 5;
+
+            while (divisibleByThree < limes) 
+            {
+                if (divisibleByThree < divisibleByFive)
+                {
+                    sum += divisibleByThree;
+                    divisibleByThree += 3;
+                }
+                else if (divisibleByThree > divisibleByFive)
+                {
+                    sum += divisibleByFive;
+                    divisibleByFive += 5;
+                }
+                
+                if (divisibleByThree == divisibleByFive)
+                {
+                    sum += divisibleByThree;
+                    divisibleByThree += 3;
+                    divisibleByFive += 5;
+                }
+            }
+
+            Assert.Equal(233168, sum);
+        }
+    }
+}
