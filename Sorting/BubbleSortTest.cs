65725d25d7ee6a5cb143f1b33ca3e147081dfcc9
Lukasz Lepecki <llepecki@users.noreply.github.com>
2019-04-15 21:42:19 +0200

    Test data are now read from csv file

---
+++ Sorting/BubbleSortTest.cs
@@ -0,0 +1,9 @@
+namespace Com.Lepecki.AlgorithmicPlayground.Sorting
+{
+    public class BubbleSortTest : SortTest
+    {
+        protected override void Sort(int[] array)
+        {
+        }
+    }
+}
--- Sorting/SortTestData.cs
+++ Sorting/SortTestData.cs
@@ -1,10 +1,46 @@
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
 namespace Com.Lepecki.AlgorithmicPlayground.Sorting
 {
     public class SortTestData : SortTheoryData
     {
         public SortTestData()
         {
-            AddRow(new[] { 3, 4, 1, 6, 5, 2 }, new[] { 1, 2, 3, 4, 5, 6 });
+            Stream resource = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(SortTestData).FullName}.csv");
+
+            using (var reader = new StreamReader(resource, Encoding.UTF8))
+            {
+                bool dataRow = false;
+
+                while (!reader.EndOfStream)
+                {
+                    if (dataRow)
+                    {
+                        string[] testData = reader.ReadLine()?.Split(',');
+
+                        if (testData != null)
+                        {
+                            int[] unsorted = ExtractNumbers(testData[0]);
+                            int[] sorted = ExtractNumbers(testData[1]);
+                            
+                            AddRow(unsorted, sorted);
+                        }
+                    }
+                    else
+                    {
+                        reader.ReadLine();
+                        dataRow = true;
+                    }
+                }
+            }
+        }
+
+        private int[] ExtractNumbers(string stringData)
+        {
+            return stringData.Split(' ').Select(int.Parse).ToArray();
         }
     }
 }
