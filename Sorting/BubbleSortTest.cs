6ba9780a3e4aa7362e0a9eaef16a8afc5b0cb92f
Lukasz Lepecki <llepecki@users.noreply.github.com>
2019-04-15 22:25:25 +0200

    Implemented BubbleSort

--- Sorting/BubbleSortTest.cs
+++ Sorting/BubbleSortTest.cs
@@ -4,6 +4,18 @@ namespace Com.Lepecki.AlgorithmicPlayground.Sorting
     {
         protected override void Sort(int[] array)
         {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        int tmp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = tmp;
+                    }
+                }
+            }
         }
     }
 }
